742fbd20589143adfab8d2966aadf98c4c100703
lavanya <lavanya@infrrd.ai>
2019-07-09 14:49:37 +0530

    intial code changes for movie-info-service

---
+++ movie-info-service/src/main/java/com/lavanya/movieinfoservice/resources/MovieResource.java
@@ -0,0 +1,19 @@
+package com.lavanya.movieinfoservice.resources;
+
+import com.lavanya.movieinfoservice.models.Movie;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/movies")
+public class MovieResource {
+
+    @RequestMapping("/{movieId}")
+    public Movie getMovieInfo(@PathVariable("movieId") String movieId) {
+        //MovieSummary movieSummary = restTemplate.getForObject("https://api.themoviedb.org/3/movie/" + movieId + "?api_key=" +  apiKey, MovieSummary.class);
+        return new Movie(movieId, "Test", "xyx");
+
+    }
+
+}
